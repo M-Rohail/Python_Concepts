766c269501b934571dae6f3e77a43ec3c924cab6
Rohail <rohailqurexhi64@gmail.com>
2023-02-05 17:40:11 +0500

    useful functions of Math

---
+++ Useful function numbers.py
@@ -0,0 +1,13 @@
+import math
+pi = -3.14
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi, 2))
+print(math.sqrt(340))
+x = 1
+y = 5
+z = 3
+print(max(x, y, z))
+print(min(x, y, z))
