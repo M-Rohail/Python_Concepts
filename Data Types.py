55f3389bf47b26ba51fb79094f93645f68449fc1
Rohail <rohailqurexhi64@gmail.com>
2023-02-05 16:22:34 +0500

    This is How data types work

---
+++ Data Types.py
@@ -0,0 +1,20 @@
+# Data Types
+print("Hello")
+print("Hey There")
+print("I love Pizza")
+x = 20
+y = "1"
+z = "Ali Bhai"
+x = "3"
+print(x, y, z)
+print(type(x))
+print(x+y)
+height = 20.6
+print("Your height is " + str(height)+" Cm.")
+# type casting
+human = False
+print(human)
+print(type(human))
+print("Are you a human "+str(human))
+print(type(human))
+# This is how variable run in python
