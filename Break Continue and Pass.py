e3934f284ad78d4808859852afc10fecd3e55833
Rohail <rohailqurexhi64@gmail.com>
2023-02-06 03:13:13 +0500

    Switch statment

---
+++ Break Continue and Pass.py
@@ -0,0 +1,10 @@
+# while True:
+#     name = input("Enter your name :")
+#     if name != "":
+#         break
+
+phone = "123-456-789"
+for i in phone:
+    if i == "-":
+        continue
+    print(i)
