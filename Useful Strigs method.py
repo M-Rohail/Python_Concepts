83278512c07f1e0d8af4dbbf861cccb11e0b6a60
Rohail <rohailqurexhi64@gmail.com>
2023-02-05 17:03:25 +0500

    String methods e.g(Upper, Lower)

---
+++ Useful Strigs method.py
@@ -0,0 +1,11 @@
+name = "Rohail"
+print(len(name))
+print(name.find("o"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("o"))
+print(name.replace("i", "a"))
+print(name*3)
