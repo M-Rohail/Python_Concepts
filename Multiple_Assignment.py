5797b488022304a6257cbb50adff3a9811715a87
Rohail <rohailqurexhi64@gmail.com>
2023-02-05 16:52:09 +0500

    Multiple Assignment

---
+++ Multiple_Assignment.py
@@ -0,0 +1,8 @@
+# name = "Bro"
+# age = 21
+# attractive = True
+name, age, attractive = "Broo", 22, False
+print(name)
+print(age)
+print(attractive)
+# It allow us to assign multiple variable at the same time in one line of code
