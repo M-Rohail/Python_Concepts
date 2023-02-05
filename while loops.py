729a7990b2170180c7c337e88ba273e3f92733dc
Rohail <rohailqurexhi64@gmail.com>
2023-02-06 02:02:26 +0500

    While and for loops

---
+++ while loops.py
@@ -0,0 +1,19 @@
+# name = ""
+
+# while len(name) == 0:
+#     name = input("Enter Your name:")
+
+#     print(name)
+
+# For Loops
+
+# for i in range(50, 100, 2):
+#     print(i)
+
+# for i in "Rohail":
+#     print(i)
+import time
+for seconds in range(1, 10, +2):
+    print(seconds)
+    time.sleep(1)
+print("Happy new year!")
