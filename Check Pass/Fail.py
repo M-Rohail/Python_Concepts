4f2831524978425f63143cb7803d0d4ba66c9d35
Rohail <rohailqurexhi64@gmail.com>
2023-02-12 02:55:23 +0500

    Basics

---
+++ Check Pass/Fail.py
@@ -0,0 +1,11 @@
+# Create a program to check whether a student passed or failed his/her examination.
+
+# Get integer input marks.
+# Check if the mark is greater than or equal to 40.
+# If it is greater than or equal to 40, print Pass.
+# Else, print Fail.
+marks = int(input("Enter the marks:"))
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
