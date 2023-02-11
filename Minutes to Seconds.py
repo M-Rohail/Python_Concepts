e650f0e2118e08f67540c7858b6b29eb5bd15be3
Rohail <rohailqurexhi64@gmail.com>
2023-02-11 05:02:32 +0500

    Time Cinverter

---
+++ Minutes to Seconds.py
@@ -0,0 +1,8 @@
+# Create a program to convert minutes into seconds.
+
+# Get an integer input time_minutes.
+# Convert time_minutes into seconds by multiplying it with 60. (1 minute = 60 seconds).
+# Print the result in seconds.
+time_input = int(input("Enter the minutes :"))
+result = time_input*60
+print(result)
