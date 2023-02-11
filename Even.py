864c460184597335a08969ba25474355605efb7f
Rohail <rohailqurexhi64@gmail.com>
2023-02-12 03:25:42 +0500

    Basics

---
+++ Even.py
@@ -0,0 +1,12 @@
+# Write a program to check if the entered integer is odd or even.
+
+# Enter an integer input for the variable number.
+# Check if it is even or odd using if...else
+# If the integer is exactly divisible by 2, print Even. Else, print Odd.
+
+
+var = int(input("Enter the no:"))
+if var % 2 == 0:
+    print("even")
+else:
+    print("Odd")
