1e934d9cf15cff797efe2ef1a106710849df139d
Rohail <rohailqurexhi64@gmail.com>
2023-02-12 02:36:34 +0500

    Basic Problem

---
+++ Average no.py
@@ -0,0 +1,13 @@
+# Create a program to find the average of three numbers entered by the user.
+
+# The average of 3 numbers a, b, and c is given by:
+
+# average = (a + b + c) / 3
+# Take float input for three variables number1, number2, and number3.
+# Calculate the average using the formula and print it.
+
+number1 = float(input("Enter the no:"))
+number2 = float(input("Enter the no:"))
+number3 = float(input("Enter the no:"))
+average = (number1+number2+number3)/3
+print(average)
