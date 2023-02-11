a8cafa256436641aa4f1c73c57b3f57ab436adb8
Rohail <rohailqurexhi64@gmail.com>
2023-02-12 02:32:18 +0500

    Just practicing

---
+++ Area of triangle.py
@@ -0,0 +1,13 @@
+# Create a program to find the area of a right angled triangle.
+
+# The area of a right angled triangle is given by:
+
+# area = 1/2 * base * height
+# Take two float inputs for base and height variables.
+# Calculate the area of the triangle using the given formula and assign it to area.
+# Print the value of area.
+
+base = float(input("Enter the base :"))
+height = float(input("Enter the height :"))
+area = 1/2*base*height
+print(area)
