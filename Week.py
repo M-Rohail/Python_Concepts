8ede58d425e08cee502f2a474349f96f5dc35ece
Rohail <rohailqurexhi64@gmail.com>
2023-02-12 03:30:59 +0500

    Basics

---
+++ Week.py
@@ -0,0 +1,26 @@
+# Write a program to print the day of the week.
+
+# Get in input integer num.
+# If num is 1, print 'Monday'.
+# If num is 2, print 'Tuesday'.
+# If num is 3, print 'Wednesday'.
+# If num is 4, print 'Thursday'.
+# If num is 5, print 'Friday'.
+# If num is 6, print 'Saturday'.
+# If num is 7, print 'Sunday'.
+
+num = int(input("Enter the no:"))
+if num == 1:
+    print("Monday")
+elif num == 2:
+    print("Tuesday")
+elif num == 3:
+    print("Wednesday")
+elif num == 4:
+    print("Thursday")
+elif num == 5:
+    print("Friday")
+elif num == 6:
+    print("Saturday")
+elif num == 7:
+    print("Sunday")
