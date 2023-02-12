b3d4fff06c58a05253bc75899df96d4bff814c39
Rohail <rohailqurexhi64@gmail.com>
2023-02-13 02:46:11 +0500

    BAsics

---
+++ Date Format.py
@@ -0,0 +1,7 @@
+# Create a program to print the date in the given format.
+# Get three string inputs: day, month, and year.
+# Print them in this format: month/day/year
+day = input("Enter a day:")
+month = input("Enter a month:")
+year = input("Enter the year:")
+print(f"{month}/{day}/{year}")
