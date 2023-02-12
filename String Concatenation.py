51658cbb7662b07fe9016c358f9187b09e445a8e
Rohail <rohailqurexhi64@gmail.com>
2023-02-13 02:41:29 +0500

    Basics

---
+++ String Concatenation.py
@@ -0,0 +1,9 @@
+# Write a program to concatenate two strings.
+# Get two string inputs for variables string1 and string2.
+# Concatenate these strings and store them in the result variable.
+# Print the value of result.
+
+string1 = input("Enter the text:")
+string2 = input("Enter the text:")
+value = string1+" "+string2
+print(value)
