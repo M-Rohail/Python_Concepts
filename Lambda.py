23a9cc74b40c02ee19c3244f53379910e3499e25
Rohail <rohailqurexhi64@gmail.com>
2023-02-12 23:55:00 +0500

    Lambda functions

---
+++ Lambda.py
@@ -0,0 +1,24 @@
+# Create a lambda function that returns the square of a number passed as an argument.
+
+# Create the lambda function and assign it to a variable named square.
+# Take a float input from the user and assign it to the number variable.
+# Call the lambda function with number as an argument and print the result.
+
+
+# number = float(input("Enter the no:"))
+# def squ(n): return n**2
+
+
+# print(squ(number))
+
+# Create a lambda function that takes two arguments and returns their sum.
+
+# Create the lambda function and assign it to a variable named add.
+# Take two integer inputs and assign them to n1 and n2 respectively.
+# Call the lambda function with n1 and n2 as arguments and print the result.
+n1 = int(input("Enter the no:"))
+n2 = int(input("Enter the no"))
+def sum(n, n1): return n+n1
+
+
+print(sum(n1, n2))
