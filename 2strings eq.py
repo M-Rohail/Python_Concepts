c4acb6279d684e61698c1fdfd02e2d9af4e3112b
Rohail <rohailqurexhi64@gmail.com>
2023-02-13 03:20:24 +0500

    basics

---
+++ 2strings eq.py
@@ -0,0 +1,17 @@
+# Write a program to check if two strings are equal or not.
+
+# Take two string inputs and assign them to str1 and str2 respectively.
+# Convert both strings to lowercase and check if they are equal using ==
+# If two strings are equal print Equal else print Not Equal.
+c1 = input("Enter the text:")
+c2 = input("Enter the text:")
+var = False
+
+for i in range(0,):
+    if c1[i].lower() == c2[i].lower():
+        var = True
+
+if var == True:
+    print("Equal")
+else:
+    print("Not Equal")
