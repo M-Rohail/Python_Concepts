bc9abb614e0de2a0c14fe324a9ed721e9b21d5b8
Rohail <rohailqurexhi64@gmail.com>
2023-02-13 00:29:23 +0500

    Basics

---
+++ List product.py
@@ -0,0 +1,12 @@
+# Write a program to calculate the product of list items.
+
+# Create a list my_list and assign [2, 5, 3, 4, 1] to it.
+# Declare a variable product with value 1.
+# Iterate over my_list and multiply each item to product.
+# Print product.
+
+my_list = [2, 5, 3, 4, 1]
+product = 1
+for i in my_list:
+    product = product*i
+print(product)
