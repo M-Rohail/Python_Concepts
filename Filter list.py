2bcc3c679f70002194d52eb02cb0350b2cd6f2bc
Rohail <rohailqurexhi64@gmail.com>
2023-02-15 00:29:25 +0500

    Basics oop

---
+++ Filter list.py
@@ -0,0 +1,13 @@
+# Create a program to create a list of odd numbers from a list of numbers using list comprehension.
+
+# Create a list with the following data items 12, 17, 28, 19, 11, and assign it to the numbers variable.
+# Create a new list and only print 17, 19, 11 (odd numbers) using list comprehension.
+# Print newly created list.
+
+numbers = [12, 17, 28, 19, 11]
+newlist = []
+for i in numbers:
+    if i % 2 == 1:
+        newlist.append(i)
+
+print(newlist)
